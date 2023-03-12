42babcfe5b1cad91fde120166d8cb6a601c59a60
Harsha Guda <harshavardhan.guda@gmail.com>
2023-03-12 11:20:52 +0200

    Use picamera2 for image publishing.

--- catkin_ws/src/perception/sign_light_ped_detection_ssd/src/utilities/cam_pub.py
+++ catkin_ws/src/perception/sign_light_ped_detection_ssd/src/utilities/cam_pub.py
@@ -6,23 +6,24 @@ import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+from picamera2 import Picamera2
+
 class CameraPub():
     def __init__(self):
         rospy.init_node('Campubnode', anonymous=False)     
         self.publisher = rospy.Publisher("/automobile/image_raw", Image, queue_size=1)
+        self.picam2 = Picamera2()
+        self.picam2.configure(self.picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+        self.picam2.start()
+
     def run(self):
-        cap = cv2.VideoCapture(0)
-        if not cap.isOpened():
-                print("Cannot open camera")
-                exit()
-        else:
-                ret, _ = cap.read()
-        while ret:
-                ret, image = cap.read()
-                imageObject = CvBridge().cv2_to_imgmsg(image, "bgr8")
-                imageObject.header.stamp = rospy.Time.now()
-                self.publisher.publish(imageObject)
-        cap.release()
+        while True:
+            image = self.picam2.capture_array()
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            imageObject = CvBridge().cv2_to_imgmsg(image, "bgr8")
+            imageObject.header.stamp = rospy.Time.now()
+            self.publisher.publish(imageObject)
+        
 
 if __name__ == '__main__':
     try:
